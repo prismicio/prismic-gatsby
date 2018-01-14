9ce90fe00dc5beb326ddd1223a8fe73277fc1418
Angelo Ashmore <angeloashmore@users.noreply.github.com>
2018-01-14 11:25:33 -1000

    Convert project to use gatsby-node-helpers

--- src/nodes.js
+++ src/nodes.js
@@ -1,41 +1,9 @@
-import { camelCase, upperFirst } from 'lodash'
-import { createHash } from 'crypto'
-import stringify from 'json-stringify-safe'
+import createNodeHelpers from 'gatsby-node-helpers'
 
-const sourceId = `__SOURCE__`
-const typePrefix = `Prismic`
-const conflictFieldPrefix = `prismic`
-const restrictedNodeFields = [`id`, `children`, `parent`, `fields`, `internal`]
+const { createNodeFactory } = createNodeHelpers({
+  typePrefix: `Prismic`
+})
 
-const makeTypeName = type => upperFirst(camelCase(`${typePrefix} ${type}`))
-const digest = str => createHash(`md5`).update(str).digest(`hex`)
+const DOCUMENT_TYPE = `Document`
 
-// DocumentNode
-//
-// Each document is a DocumentNode. Data is available in the `data` property.
-export const DocumentNode = ({ documentItem: documentItemOriginal }) => {
-  const documentItem = Object.assign({}, documentItemOriginal)
-
-  // Prefix conflicting keys.
-  Object.keys(documentItem).forEach(key => {
-    if (restrictedNodeFields.includes(key)) {
-      documentItem[conflictFieldPrefix + upperFirst(key)] = documentItem[key]
-      delete documentItem[key]
-    }
-  })
-
-  // Need to use prismicId since the original id key conflicts with Gatsby.
-  const node = {
-    ...documentItem,
-    id: documentItem.prismicId,
-    parent: sourceId,
-    children: [],
-    internal: {
-      type: makeTypeName(`Document`),
-    },
-  }
-
-  node.internal.contentDigest = digest(stringify(node))
-
-  return node
-}
+export const DocumentNode = createNodeFactory(DOCUMENT_TYPE)
