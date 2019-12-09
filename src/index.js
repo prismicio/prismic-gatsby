d53f8aa92b0fb204cc7088bf47784252a7133d5a
Angelo Ashmore <angeloashmore@users.noreply.github.com>
2019-12-08 17:30:54 -1000

    feat: export getFixedGatsbyImage and getFluidGatsbyImage

--- src/index.js
+++ src/index.js
@@ -2,3 +2,8 @@ export {
   usePrismicPreview,
   mergePrismicPreviewData,
 } from './browser/usePrismicPreview'
+
+export {
+  getFixedGatsbyImage,
+  getFluidGatsbyImage,
+} from './common/getGatsbyImage'
