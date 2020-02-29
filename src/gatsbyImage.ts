dad48b940dd9b1ee3b181191f6c5143e82984fdc
Angelo Ashmore <angeloashmore@users.noreply.github.com>
2020-02-29 13:14:15 -1000

    fix: use correct scaled height for fluid

--- src/gatsbyImage.ts
+++ src/gatsbyImage.ts
@@ -82,6 +82,7 @@ const buildFixedSrcSet = (
 
 const buildFluidSrcSet = (
   baseURL: string,
+  aspectRatio: number,
   params: ImgixUrlQueryParams & { w: number; rect?: string },
   breakpoints?: number[],
 ) => {
@@ -96,7 +97,11 @@ const buildFluidSrcSet = (
   return uniqSortedBreakpoints
     .map(breakpoint => {
       if (!breakpoint) return
-      const url = buildURL(baseURL, { ...params, w: breakpoint })
+      const url = buildURL(baseURL, {
+        ...params,
+        w: breakpoint,
+        h: Math.round(breakpoint / aspectRatio),
+      })
       return `${url} ${breakpoint}w`
     })
     .filter(Boolean)
@@ -157,6 +162,7 @@ export const buildFluidGatsbyImage = (
   const src = buildURL(baseURL, { w: width, h: height, rect, q: quality })
   const srcSet = buildFluidSrcSet(
     baseURL,
+    aspectRatio,
     { w: width, h: height, rect, q: quality },
     breakpoints,
   )
