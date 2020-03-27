5d45740ecc9c51855ea7f6a60281adc020f1e1f1
Vincent Velociter <vincent.velociter@gmail.com>
2020-03-27 17:33:29 +0100

    fix: import ssr function from dist

--- gatsby-ssr.js
+++ gatsby-ssr.js
@@ -1,11 +1,3 @@
-function _interopDefault (ex) { return (ex && (typeof ex === 'object') && 'default' in ex) ? ex['default'] : ex; }
+const ssr = require('./dist/gatsby-ssr')
 
-var React = _interopDefault(require('react'));
-
-exports.onRenderBody = ({ setHeadComponents }, options) => {
-    const src = `//prismic.io/prismic.js?repo=${options.repositoryName}`;
-    const key = 'prismic-script';
-    const toolbarScript = React.createElement('script', { key, src });
-    /* TODO: make optional */
-    setHeadComponents([toolbarScript]);
-};
+exports.onRenderBody = ssr.onRenderBody
