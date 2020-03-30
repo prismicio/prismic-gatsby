07dba6a7a0a003a5a4920a0dff327734a3803117
MarcMcIntosh <marcmcintosh1987@gmail.com>
2020-03-30 14:09:16 +0200

    fix: featch script using https protocol

--- src/gatsby-ssr.ts
+++ src/gatsby-ssr.ts
@@ -7,7 +7,7 @@ export interface OnRenderBodyArgs {
 
 export const onRenderBody = ({ setHeadComponents }: OnRenderBodyArgs, options: PluginOptions) => {
   if(options.omitPrismicScript) return;
-  const src = `//prismic.io/prismic.js?repo=${options.repositoryName}`;
+  const src = `https://prismic.io/prismic.js?repo=${options.repositoryName}`;
   const key = 'prismic-script';
   const toolbarScript = React.createElement('script', { key, src });
   /* TODO: make optional */
