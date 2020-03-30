eea005a2576f1353c172ecb0d19f84ca0c588ea6
MarcMcIntosh <marcmcintosh1987@gmail.com>
2020-03-30 14:49:31 +0200

    fix: load and configure toolbar correctly

--- src/gatsby-ssr.ts
+++ src/gatsby-ssr.ts
@@ -7,16 +7,10 @@ export interface OnRenderBodyArgs {
 
 export const onRenderBody = ({ setHeadComponents }: OnRenderBodyArgs, options: PluginOptions) => {
   if(options.omitPrismicScript) return;
-  const settings = React.createElement('script',{
-    children: `window.prismic = { endpoint: "https://${options.repositoryName}.cdn.prismic.io/api/v2"}`
-  });
 
-  const src = `https://static.cdn.prismic.io/prismic.min.js?&new=true`;
+  const src = `//static.cdn.prismic.io/prismic.js?repo=${options.repositoryName}&new=true`;
   const key = 'prismic-script';
-  const toolbarScript = React.createElement('script', { key, src });
-  /* TODO: make optional */
-  setHeadComponents([
-    settings,
-    toolbarScript,
-  ]);
+  const toolbarScript = React.createElement('script', { key, src, async: true, defer: true });
+
+  setHeadComponents([ toolbarScript ]);
 }
