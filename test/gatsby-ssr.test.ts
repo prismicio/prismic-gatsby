e67be2b570b2e768d749557727ba5ecd86dc24be
Angelo Ashmore <angeloashmore@users.noreply.github.com>
2020-04-05 12:09:44 -1000

    style: run prettier

--- test/gatsby-ssr.test.ts
+++ test/gatsby-ssr.test.ts
@@ -1,44 +1,35 @@
-import {
-    onRenderBody,
-} from "../src/gatsby-ssr";
-import { PluginOptions } from '../src/types';
+import { onRenderBody } from '../src/gatsby-ssr'
+import { PluginOptions } from '../src/types'
 
+describe('onRenderBody', () => {
+  test('expect setHeadComponents to not have been called', () => {
+    const repositoryName: string = 'foo'
+    const setHeadComponents: jest.Mock<any, any> = jest.fn()
 
-describe("onRenderBody", () => {
+    const options: PluginOptions = {
+      repositoryName,
+      plugins: [],
+      schemas: { page: {} },
+    }
 
-    test("expect setHeadComponents to not have been called", () => {
-        const repositoryName: string = "foo";
-        const setHeadComponents: jest.Mock<any, any>= jest.fn();
-  
-        const options: PluginOptions = {
-            repositoryName,
-            plugins: [],
-            schemas: { page: {} },
-        };
+    onRenderBody({ setHeadComponents }, options)
 
-        onRenderBody({ setHeadComponents }, options);
+    expect(setHeadComponents).toBeCalledTimes(1)
+  })
 
-        expect(setHeadComponents).toBeCalledTimes(1)
-        
+  test('expect setHeadComponents to have been called', () => {
+    const repositoryName: string = 'foo'
+    const setHeadComponents: jest.Mock<any, any> = jest.fn()
 
-    });
+    const options: PluginOptions = {
+      repositoryName,
+      plugins: [],
+      schemas: { page: {} },
+      prismicScript: true,
+    }
 
-    test("expect setHeadComponents to have been called", () => {
-        const repositoryName: string = "foo";
-        const setHeadComponents: jest.Mock<any, any>= jest.fn();
-  
-        const options: PluginOptions = {
-            repositoryName,
-            plugins: [],
-            schemas: { page: {} },
-            prismicScript: true,
-        };
+    onRenderBody({ setHeadComponents }, options)
 
-        onRenderBody({ setHeadComponents }, options);
-
-        expect(setHeadComponents).not.toHaveBeenCalled
-        
-
-    });
-
-});
+    expect(setHeadComponents).not.toHaveBeenCalled
+  })
+})
