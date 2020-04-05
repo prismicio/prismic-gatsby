f5750573122cb574493dd567653141cbe1c51708
Angelo Ashmore <angeloashmore@users.noreply.github.com>
2020-04-05 12:21:41 -1000

    fix: correct prismicToolbar option name

--- src/gatsby-ssr.ts
+++ src/gatsby-ssr.ts
@@ -1,15 +1,15 @@
-import { RenderBodyArgs } from 'gatsby'
+import { GatsbySSR, RenderBodyArgs } from 'gatsby'
 import React from 'react'
 
 import { PluginOptions } from './types'
 
-export const onRenderBody = (
+export const onRenderBody: GatsbySSR['onRenderBody'] = async (
   gatsbyContext: RenderBodyArgs,
   options: PluginOptions,
 ) => {
   const { setHeadComponents } = gatsbyContext
 
-  if (!options.prismicScript) return
+  if (!options.prismicToolbar) return
 
   const toolbarScript = React.createElement('script', {
     src: `//static.cdn.prismic.io/prismic.js?repo=${options.repositoryName}&new=true`,
