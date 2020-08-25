0beada8a665d9c2ca7ef8bdf4c236a096da0d95c
MarcMcIntosh <marcmcintosh1987@gmail.com>
2020-08-25 17:28:01 +0200

    feat: handle addition webhooks for main api and release

--- src/webhook.ts
+++ src/webhook.ts
@@ -1,4 +1,9 @@
-import { WebhookBase, PluginOptions } from 'types';
+import { SourceNodesArgs } from 'gatsby'
+import { WebhookBase, PluginOptions, PrismicWebhook, TypePath, WebhookDocument } from 'types';
+import { fetchDocumentsByIds } from './api'
+import { documentsToNodes } from './documentsToNodes'
+import { createEnvironment } from './environment.node'
+import { msg } from './utils'
 
 type maybeWebhook = WebhookBase | any;
 
@@ -17,8 +22,71 @@ export function isPrismicUrl(url: string | undefined): boolean {
 
 
 export function isPrismicWebhook(webhookBody: maybeWebhook): boolean {
+
   if(!webhookBody) return false;
+
   if(typeof webhookBody !== "object") return false;
+
   if(webhookBody.type === "test-trigger") return false;
+
   return isPrismicUrl(webhookBody.apiUrl)
-} 
+}
+
+
+export async function handleWebhook(pluginOptions: PluginOptions, gatsbyContext: SourceNodesArgs, typePaths: TypePath[], webhook: PrismicWebhook) {
+  const { releaseID } = pluginOptions
+  const { reporter } = gatsbyContext
+  
+  reporter.info(msg("Processing webhook"))
+
+  // eventually we could handle changes to mask and custom types here :)
+
+  const mainApiAdditions = (webhook.documents.addition || [])
+
+  const releaseAdditions = [
+    ...webhook.releases.update || [],
+    ...webhook.releases.addition || []
+  ].reduce((acc, release) => {
+    if(release.id !== releaseID) return acc;
+
+    return [
+      ...acc,
+      ...(release.documents.addition || []),
+      // ...release.document.update || [], // not part of document spec at the moment
+    ]
+
+  }, [] as WebhookDocument[])
+
+
+  /* const releaseRemovals = (prismicWebhook.releases.deletion || []).map(doc => doc.id)
+  const mainApiRemovals = (prismicWebhook.documents.deletion || []).map(doc => doc.id) */
+
+  const buildRelease = (releaseID && process.env.GATSBY_ENV === 'development')
+ 
+  const documentsToAdd = buildRelease ? [...releaseAdditions, ...mainApiAdditions] : mainApiAdditions
+
+  if(documentsToAdd.length) {
+    await handleWebhookUpdates(pluginOptions, gatsbyContext, typePaths, documentsToAdd);
+  }
+  
+  reporter.info(msg("Processed webhook"))
+}
+
+export async function handleWebhookUpdates(pluginOptions: PluginOptions, gatsbyContext: SourceNodesArgs, typePaths: TypePath[], documents: WebhookDocument[]) {
+
+  const { reporter } = gatsbyContext
+
+  reporter.info(msg(`Updating ${documents.length} documents`))
+  
+  const newDocs = await fetchDocumentsByIds(pluginOptions, gatsbyContext, documents)
+  
+  const env = createEnvironment(pluginOptions, gatsbyContext, typePaths)
+
+  const processedDocuments = await documentsToNodes(newDocs, env)
+
+  reporter.info(msg(`Updated ${processedDocuments.length} documents`))
+}
+
+export async function handleWebhookDeletions(...args: any) {
+  return args;
+}
--- test/webhooks.test.ts
+++ test/webhooks.test.ts
@@ -1,6 +1,10 @@
-import { validateSecret, isPrismicUrl, isPrismicWebhook } from '../src/webhook'
-import { PluginOptions } from '../src/types'
-import testTrigger from './__fixtures__/webhooks/test-trigger.json'
+import { SourceNodesArgs } from 'gatsby'
+import mockSchema from './__fixtures__/schema.json'
+import { validateSecret, isPrismicUrl, isPrismicWebhook, handleWebhook } from '../src/webhook'
+import { PluginOptions, Schema } from '../src/types'
+import { testTrigger , mainApiAddition, releaseAddition } from './__fixtures__/webhooks'
+import { schemasToTypeDefs } from '../src/schemasToTypeDefs'
+
 
 describe("validadteSecret", () => {
   it("should return true when secret is not configured on both sides", () => {
@@ -93,4 +97,164 @@ describe("isPrismicWebhook", () => {
       apiUrl: "https://example.com",
     })).toBe(false)
   })
+
+  it("should return true when receiving a webhook from prismic", () => {
+    const result = isPrismicWebhook(mainApiAddition)
+    expect(result).toBe(true)
+  })
 })
+
+describe("handleWebhook", () => {
+  beforeEach(() => {
+    jest.clearAllMocks()
+    if (process.env.GATSBY_ENV) {
+      delete process.env.GATSBY_ENV;
+    }
+  })
+
+  it("should call createNode when passed a webhook for main api addition", async () => {
+    const pluginOptions = {
+      repositoryName: 'repositoryName',
+      plugins: [],
+      schemas: { page: mockSchema as Schema },
+    }
+
+    const gatsbyContext = createGatsbyContext();
+    const { typePaths } = schemasToTypeDefs(
+      pluginOptions.schemas,
+      gatsbyContext,
+    )
+    await handleWebhook(pluginOptions, gatsbyContext, typePaths, mainApiAddition)
+
+    expect(gatsbyContext.actions.createNode).toBeCalled()
+  })
+
+  it("should not call createNode when passed a webhook for a release addition and the the releaseID is unconfigured", async () => {
+
+    process.env.GATSBY_ENV = 'development';
+
+    const pluginOptions = {
+      repositoryName: 'repositoryName',
+      plugins: [],
+      schemas: { page: mockSchema as Schema },
+    }
+
+    const gatsbyContext = createGatsbyContext();
+    const { typePaths } = schemasToTypeDefs(
+      pluginOptions.schemas,
+      gatsbyContext,
+    )
+    await handleWebhook(pluginOptions, gatsbyContext, typePaths, releaseAddition)
+
+    expect(gatsbyContext.actions.createNode).not.toBeCalled()
+  })
+
+  it("should not call createNode when passed a webhook for a release addition and the releaseID does not match the configured id", async () => {
+
+    process.env.GATSBY_ENV = 'development';
+
+    const pluginOptions: PluginOptions = {
+      repositoryName: 'repositoryName',
+      plugins: [],
+      schemas: { page: mockSchema as Schema },
+      releaseID: "qwertyuiop"
+    }
+
+    const gatsbyContext = createGatsbyContext();
+    const { typePaths } = schemasToTypeDefs(
+      pluginOptions.schemas,
+      gatsbyContext,
+    )
+    await handleWebhook(pluginOptions, gatsbyContext, typePaths, releaseAddition)
+
+    expect(gatsbyContext.actions.createNode).not.toBeCalled()
+  })
+
+  it("should call createNode when passed a webhook for a release addition and the releaseID match", async () => {
+
+    process.env.GATSBY_ENV = "development";
+
+    const pluginOptions: PluginOptions = {
+      repositoryName: 'repositoryName',
+      plugins: [],
+      schemas: { page: mockSchema as Schema },
+      releaseID: "XyfxIPl3p7YAQ7Mg"
+    }
+
+    const gatsbyContext = createGatsbyContext();
+    const { typePaths } = schemasToTypeDefs(
+      pluginOptions.schemas,
+      gatsbyContext,
+    )
+    await handleWebhook(pluginOptions, gatsbyContext, typePaths, releaseAddition)
+
+    expect(gatsbyContext.actions.createNode).toBeCalled()
+  })
+
+  it("should not call createNode when passed a webhook for a release addition and GATSBY_ENV is not 'development", async () => {
+    
+    const pluginOptions: PluginOptions = {
+      repositoryName: 'repositoryName',
+      plugins: [],
+      schemas: { page: mockSchema as Schema },
+      releaseID: "XyfxIPl3p7YAQ7Mg"
+    }
+
+    const gatsbyContext = createGatsbyContext();
+    const { typePaths } = schemasToTypeDefs(
+      pluginOptions.schemas,
+      gatsbyContext,
+    )
+    await handleWebhook(pluginOptions, gatsbyContext, typePaths, releaseAddition)
+
+    expect(gatsbyContext.actions.createNode).not.toBeCalled()
+  })
+})
+
+
+function createGatsbyContext() {
+  const PROGRAM_DIRECTORY_PATH = '/__PROGRAM_DIRECTORY__/';
+  const mockGatsbyContext: SourceNodesArgs = {
+    // @ts-expect-error - partial implementation
+    actions: {
+      createTypes: jest.fn(),
+      createNode: jest.fn(),
+    },
+    // @ts-expect-error - partial implementation
+    store: {
+      getState: jest
+        .fn()
+        .mockReturnValue({ program: { directory: PROGRAM_DIRECTORY_PATH } }),
+    },
+    // @ts-expect-error - partial implementation
+    reporter: {
+      activityTimer: jest
+        .fn()
+        .mockReturnValue({ start: jest.fn(), end: jest.fn() }),
+      error: jest.fn(),
+      panic: jest.fn<never, any>((msg: string | Error, err?: Error) => {
+        throw err ?? msg
+      }),
+      verbose: jest.fn(),
+      info: jest.fn()
+    },
+    // @ts-expect-error - partial implementation
+    schema: {
+      buildObjectType: jest
+        .fn()
+        .mockImplementation((config) => ({ kind: 'OBJECT', config })),
+      buildUnionType: jest.fn().mockImplementation((config) => ({
+        kind: 'UNION',
+        config,
+      })),
+    },
+    createNodeId: jest.fn().mockReturnValue('createNodeId'),
+    createContentDigest: jest.fn().mockReturnValue('createContentDigest'),
+    // @ts-expect-error - partial implementation
+    cache: {
+      get: jest.fn(),
+    },
+  }
+
+  return mockGatsbyContext
+}
