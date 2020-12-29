ea62cbd3b4e68c20a3273bda590b79cd7b44c6b7
Angelo Ashmore <angeloashmore@users.noreply.github.com>
2020-12-28 17:36:05 -1000

    test: more explicit schema tests

--- packages/gatsby-source-prismic/test/create-schema-customization.test.ts
+++ packages/gatsby-source-prismic/test/create-schema-customization.test.ts
@@ -1,16 +1,99 @@
 import { createSchemaCustomization } from '../src/gatsby-node'
 import { gatsbyContext } from './__fixtures__/gatsbyContext'
 import { pluginOptions } from './__fixtures__/pluginOptions'
+import pageNode from './__fixtures__/pageNode.json'
+
+const findCreateTypesCall = (
+  name: string,
+  gatsbyCtx: typeof gatsbyContext = gatsbyContext,
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+): any =>
+  gatsbyCtx.actions.createTypes.mock.calls.find(
+    (call) => call[0].config.name === name,
+  )[0]
 
 beforeEach(() => {
   jest.clearAllMocks()
 })
 
-describe('createSchemaCustomization', () => {
-  test('creates types', async () => {
-    // @ts-expect-error - Partial gatsbyContext provided
-    await createSchemaCustomization(gatsbyContext, pluginOptions)
+test('creates types', async () => {
+  // @ts-expect-error - Partial gatsbyContext provided
+  await createSchemaCustomization(gatsbyContext, pluginOptions)
 
-    expect(gatsbyContext.actions.createTypes).toMatchSnapshot()
+  expect(gatsbyContext.actions.createTypes).toMatchSnapshot()
+})
+
+describe('shared global types', () => {
+  test.skip('creates link_types type', async () => {
+    // TODO
   })
+
+  test.skip('creates image dimensions type', async () => {
+    // TODO
+  })
+
+  test.skip('creates geopoint type', async () => {
+    // TODO
+  })
+})
+
+describe('shared local types', () => {
+  test.skip('creates Link type', async () => {
+    // TODO
+  })
+
+  test.skip('creates StructuredText type', async () => {
+    // TODO
+  })
+
+  test.skip('creates Image type', async () => {
+    // TODO
+  })
+})
+
+test('document base fields', async () => {
+  // @ts-expect-error - Partial gatsbyContext provided
+  await createSchemaCustomization(gatsbyContext, pluginOptions)
+
+  const pageCall = findCreateTypesCall('PrismicPrefixPage')
+
+  expect(pageCall).not.toBeUndefined()
+  expect(pageCall).toMatchObject({
+    kind: 'OBJECT',
+    config: {
+      name: 'PrismicPrefixPage',
+      fields: {
+        uid: 'String',
+        prismicId: 'ID!',
+        data: 'PrismicPrefixPageDataType',
+        dataRaw: { type: 'JSON!' },
+        first_publication_date: {
+          type: 'Date!',
+          extensions: { dateformat: {} },
+        },
+        href: 'String!',
+        lang: 'String!',
+        last_publication_date: {
+          type: 'Date!',
+          extensions: { dateformat: {} },
+        },
+        tags: '[String!]!',
+        type: 'String!',
+        url: { type: 'String' },
+        _previewable: { type: 'ID!' },
+      },
+      interfaces: ['Node'],
+      extensions: { infer: false },
+    },
+  })
+})
+
+test('_previewable field resolves to Prismic ID', async () => {
+  // @ts-expect-error - Partial gatsbyContext provided
+  await createSchemaCustomization(gatsbyContext, pluginOptions)
+
+  const pageCall = findCreateTypesCall('PrismicPrefixPage')
+  const previewableResolver = pageCall.config.fields._previewable.resolve
+
+  expect(previewableResolver(pageNode)).toBe(pageNode.prismicId)
 })
