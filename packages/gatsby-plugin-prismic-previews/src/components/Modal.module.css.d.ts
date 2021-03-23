cb12881d2d706b17719e0ea3045f0d889bbb590d
Angelo Ashmore <angeloashmore@users.noreply.github.com>
2021-03-22 19:05:29 -1000

    chore: remove unused CSS files

--- packages/gatsby-plugin-prismic-previews/src/components/Modal.module.css.d.ts
+++
@@ -1,9 +0,0 @@
-declare const styles: {
-  readonly "modal": string;
-  readonly "closeButton": string;
-  readonly "closeButtonIcon": string;
-  readonly "prismicLogo": string;
-  readonly "modalRed": string;
-};
-export = styles;
-
