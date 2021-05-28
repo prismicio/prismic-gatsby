e8edf346967b5159bfc74dc3d6ae70aa3a21ebc7
Angelo Ashmore <angeloashmore@users.noreply.github.com>
2021-05-28 09:43:59 -1000

    chore(starter): add comments in gatsby-*.js files [skip ci]

--- starters/gatsby-starter-prismic/gatsby-ssr.js
+++ starters/gatsby-starter-prismic/gatsby-ssr.js
@@ -7,8 +7,10 @@
 import * as React from 'react'
 import { PrismicPreviewProvider } from 'gatsby-plugin-prismic-previews'
 
+// Styling for the preview modals.
 import 'gatsby-plugin-prismic-previews/dist/styles.css'
 
+// Adds a shared React Context for Prismic preview sessions.
 export const wrapRootElement = ({ element }) => (
   <PrismicPreviewProvider>{element}</PrismicPreviewProvider>
 )
