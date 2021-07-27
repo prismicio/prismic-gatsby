1c7907f420e22b99d3c6009d85c443e2a00fa414
Angelo Ashmore <angeloashmore@users.noreply.github.com>
2021-07-26 14:57:34 -1000

    fix: correctly resolve fields with transformed names (#415)
    * fix(source): correctly resolve fields with transformed names
    
    * fix(previews): prevent bootstrapping or resolving more than once

--- packages/gatsby-source-prismic/src/lib/sourceNodesForDocumentIds.ts
+++ packages/gatsby-source-prismic/src/lib/sourceNodesForDocumentIds.ts
@@ -1,9 +1,10 @@
 import * as RTE from 'fp-ts/ReaderTaskEither'
 import { constVoid, pipe } from 'fp-ts/function'
 
-import { Dependencies } from '../types'
+import { Dependencies, Mutable } from '../types'
 
 import { createGloballyUniqueNodes } from './createGloballyUniqueNodes'
+import { normalizeDocuments } from './normalizeDocuments'
 import { queryDocumentsByIds } from './queryDocumentsByIds'
 
 /**
@@ -17,6 +18,9 @@ export const sourceNodesForDocumentIds = (
 ): RTE.ReaderTaskEither<Dependencies, Error, void> =>
   pipe(
     queryDocumentsByIds(documentIds),
-    RTE.chainW(createGloballyUniqueNodes),
+    RTE.chainW(normalizeDocuments),
+    RTE.chainW((docs) =>
+      createGloballyUniqueNodes(docs as Mutable<typeof docs>),
+    ),
     RTE.map(constVoid),
   )
