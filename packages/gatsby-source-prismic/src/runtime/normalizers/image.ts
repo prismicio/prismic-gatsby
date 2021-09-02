3c09fb9a8994777e2a825924f33b0c847f023f71
Angelo Ashmore <angeloashmore@users.noreply.github.com>
2021-09-02 12:57:25 -1000

    refactor: simplify image normalization

--- packages/gatsby-source-prismic/src/runtime/normalizers/image.ts
+++ packages/gatsby-source-prismic/src/runtime/normalizers/image.ts
@@ -35,19 +35,29 @@ export type NormalizeImageConfig<
 		"imageImgixParams" | "imagePlaceholderImgixParams"
 	>;
 
-type NormalizedImageBase<Value extends prismicT.ImageFieldImage> = Value & {
-	url: string | null;
-	fixed: gatsbyImage.FixedObject | null;
-	fluid: gatsbyImage.FluidObject | null;
-	gatsbyImageData: gatsbyPluginImage.IGatsbyImageData | null;
-	localFile: {
-		childImageSharp: {
-			fixed: gatsbyImage.FixedObject;
-			fluid: gatsbyImage.FluidObject;
-			gatsbyImageData: gatsbyPluginImage.IGatsbyImageData;
-		};
-	} | null;
-};
+type NormalizedImageBase<Value extends prismicT.ImageFieldImage> =
+	Value extends prismicT.FilledImageFieldImage
+		? Value & {
+				fixed: gatsbyImage.FixedObject;
+				fluid: gatsbyImage.FluidObject;
+				gatsbyImageData: gatsbyPluginImage.IGatsbyImageData;
+				localFile: {
+					publicURL: Value["url"];
+					childImageSharp: {
+						fixed: gatsbyImage.FixedObject;
+						fluid: gatsbyImage.FluidObject;
+						gatsbyImageData: gatsbyPluginImage.IGatsbyImageData;
+					};
+				};
+		  }
+		: Value extends prismicT.EmptyImageFieldImage
+		? Value & {
+				fixed: null;
+				fluid: null;
+				gatsbyImageData: null;
+				localFile: null;
+		  }
+		: never;
 
 export type NormalizedImageValue<Value extends prismicT.ImageField> =
 	NormalizedImageBase<Value> & {
@@ -63,99 +73,98 @@ type BuildImageFieldConfig<Value extends prismicT.ImageFieldImage> = {
 const buildImageField = <Value extends prismicT.ImageFieldImage>(
 	config: BuildImageFieldConfig<Value>,
 ): NormalizedImageBase<Value> => {
-	const imgixParams = {
-		...(config.value.url ? getURLSearchParams(config.value.url) : undefined),
-		...config.imageImgixParams,
-	};
-	const placeholderImgixParams = config.imagePlaceholderImgixParams;
+	if (config.value.url) {
+		const imgixParams = {
+			...getURLSearchParams(config.value.url),
+			...config.imageImgixParams,
+		};
+		const placeholderImgixParams = config.imagePlaceholderImgixParams;
 
-	const url = config.value.url ? new URL(config.value.url) : null;
+		const url = new URL(config.value.url);
 
-	const normalizedURL = url
-		? sanitizeImageURL(stripURLQueryParameters(url.toString()))
-		: null;
+		const normalizedURL = sanitizeImageURL(
+			stripURLQueryParameters(url.toString()),
+		);
 
-	const populatedUrl = url;
-	if (populatedUrl) {
+		const populatedUrl = new URL(url);
 		for (const paramKey in imgixParams) {
 			populatedUrl.searchParams.set(
 				paramKey,
 				String(imgixParams[paramKey as keyof typeof imgixParams]),
 			);
 		}
+
+		const fixed = imgixGatsbyHelpers.buildFixedObject({
+			url: normalizedURL,
+			args: {
+				width: 400,
+				imgixParams,
+				placeholderImgixParams,
+			},
+			sourceWidth: config.value.dimensions.width,
+			sourceHeight: config.value.dimensions.height,
+		});
+
+		const fluid = imgixGatsbyHelpers.buildFluidObject({
+			url: normalizedURL,
+			args: {
+				maxWidth: 800,
+				imgixParams,
+				placeholderImgixParams,
+			},
+			sourceWidth: config.value.dimensions.width,
+			sourceHeight: config.value.dimensions.height,
+		});
+
+		const gatsbyImageData = imgixGatsbyHelpers.buildGatsbyImageDataObject({
+			url: normalizedURL,
+			dimensions: config.value.dimensions,
+			defaultParams: imgixParams,
+			resolverArgs: {},
+		});
+
+		return {
+			url: sanitizeImageURL(populatedUrl.toString()),
+			alt: config.value.alt,
+			copyright: config.value.copyright,
+			dimensions: config.value.dimensions,
+			fixed,
+			fluid,
+			gatsbyImageData,
+			localFile: {
+				publicURL: config.value.url,
+				childImageSharp: {
+					fixed,
+					fluid,
+					gatsbyImageData,
+				},
+			},
+		} as NormalizedImageBase<Value>;
+	} else {
+		return {
+			url: null,
+			alt: null,
+			copyright: null,
+			dimensions: null,
+			fixed: null,
+			gatsbyImageData: null,
+			fluid: null,
+			localFile: null,
+		} as NormalizedImageBase<Value>;
 	}
-
-	const fixed =
-		normalizedURL && config.value.dimensions
-			? imgixGatsbyHelpers.buildFixedObject({
-					url: normalizedURL,
-					args: {
-						width: 400,
-						imgixParams,
-						placeholderImgixParams,
-					},
-					sourceWidth: config.value.dimensions.width,
-					sourceHeight: config.value.dimensions.height,
-			  })
-			: null;
-
-	const fluid =
-		normalizedURL && config.value.dimensions
-			? imgixGatsbyHelpers.buildFluidObject({
-					url: normalizedURL,
-					args: {
-						maxWidth: 800,
-						imgixParams,
-						placeholderImgixParams,
-					},
-					sourceWidth: config.value.dimensions.width,
-					sourceHeight: config.value.dimensions.height,
-			  })
-			: null;
-
-	const gatsbyImageData =
-		normalizedURL && config.value.dimensions
-			? imgixGatsbyHelpers.buildGatsbyImageDataObject({
-					url: normalizedURL,
-					dimensions: config.value.dimensions,
-					defaultParams: imgixParams,
-					resolverArgs: {},
-			  })
-			: null;
-
-	return {
-		url: populatedUrl ? sanitizeImageURL(populatedUrl.toString()) : null,
-		alt: config.value.alt,
-		copyright: config.value.copyright,
-		dimensions: config.value.dimensions,
-		fixed,
-		fluid,
-		gatsbyImageData,
-		localFile:
-			config.value.url && config.value.dimensions
-				? {
-						publicURL: config.value.url,
-						childImageSharp: {
-							fixed,
-							fluid,
-							gatsbyImageData,
-						},
-				  }
-				: null,
-	} as NormalizedImageBase<Value>;
 };
 
 export const image = <Value extends prismicT.ImageField>(
 	config: NormalizeImageConfig<Value>,
 ): NormalizedImageValue<Value> => {
-	const result: NormalizedImageValue<Value> = {
+	const result = {
 		...buildImageField({
 			value: config.value,
 			imageImgixParams: config.imageImgixParams,
 			imagePlaceholderImgixParams: config.imagePlaceholderImgixParams,
 		}),
 		thumbnails: {},
-	};
+	} as NormalizedImageValue<Value>;
 
 	const thumbnailNames = Object.keys(config.value).filter(
 		(key) => !PRISMIC_API_IMAGE_FIELDS.includes(key),
