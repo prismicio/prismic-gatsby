6087179f36a1ede285d665c229d9216377df0c48
Angelo Ashmore <angeloashmore@users.noreply.github.com>
2021-09-03 12:17:14 -1000

    chore: build source maps

--- packages/gatsby-source-prismic/siroc.config.js
+++ packages/gatsby-source-prismic/siroc.config.js
@@ -1,9 +1,7 @@
 import { defineSirocConfig } from "siroc";
-import postcss from "rollup-plugin-postcss";
 
 export default defineSirocConfig({
 	rollup: {
-		plugins: [postcss({ inject: false })],
 		output: { sourcemap: true },
 	},
 });
