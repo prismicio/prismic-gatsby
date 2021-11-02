94487ae4899bb66d6a056935252627b90240b042
Angelo Ashmore <angeloashmore@users.noreply.github.com>
2021-11-01 16:38:53 -1000

    test(previews): fix authorization check for latest `@prismicio/client`

--- packages/gatsby-plugin-prismic-previews/test/__testutils__/isValidAccessToken.ts
+++ packages/gatsby-plugin-prismic-previews/test/__testutils__/isValidAccessToken.ts
@@ -1,13 +1,18 @@
 import * as msw from "msw";
 
-import { createAuthorizationHeader } from "./createAuthorizationHeader";
+// import { createAuthorizationHeader } from "./createAuthorizationHeader";
 
 export const isValidAccessToken = (
 	accessToken: string | undefined,
 	req: msw.RestRequest,
 ): boolean => {
+	// TODO: Uncomment when the Authorization header can be used
+	// @see Related issue - {@link https://github.com/prismicio/issue-tracker-wroom/issues/351}
+	// return typeof accessToken === "string"
+	// 	? req.headers.get("Authorization") ===
+	// 			createAuthorizationHeader(accessToken)
+	// 	: true;
 	return typeof accessToken === "string"
-		? req.headers.get("Authorization") ===
-				createAuthorizationHeader(accessToken)
+		? req.url.searchParams.get("access_token") === accessToken
 		: true;
 };
